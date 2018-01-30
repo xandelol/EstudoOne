4b7a2df1d104e5bc8200d672dbc16b658623cb69
DESKTOP-3603Q27\aacds <aacds@msn.com>
2018-01-30 01:57:04 -0200

    Login screen finished

--- app/src/main/java/xandao/com/estudoone/LoginActivity.java
+++ app/src/main/java/xandao/com/estudoone/LoginActivity.java
@@ -1,20 +1,32 @@
 package xandao.com.estudoone;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-import com.transitionseverywhere.*;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+
 
 public class LoginActivity extends AppCompatActivity {
     Button button;
     RelativeLayout faceLogin;
     LinearLayout signup;
     RelativeLayout enterForm;
+    TextInputLayout inputEmail;
+    TextInputLayout inputPass;
+    EditText editEmail;
+    EditText editPass;
 
     boolean firstStep;
     @Override
@@ -25,7 +37,12 @@ public class LoginActivity extends AppCompatActivity {
         signup = (LinearLayout) findViewById(R.id.login_screen_signup);
         button = (Button) findViewById(R.id.login_screen_button_enter);
         enterForm = (RelativeLayout) findViewById(R.id.login_screen_enter_form);
+        inputEmail = (TextInputLayout) findViewById(R.id.login_screen_email_edit_layout);
+        inputPass = (TextInputLayout) findViewById(R.id.login_screen_pass_edit_layout);
+        editEmail = (EditText) findViewById(R.id.login_screen_email_edit);
+        editPass = (EditText) findViewById(R.id.login_screen_pass_edit);
         firstStep = true;
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
                     enterForm.animate().translationY(-1400);
                     firstStep = false;
                 }else{
-                    Toast.makeText(LoginActivity.this,"ENTRAR EM BREVE!!!", Toast.LENGTH_SHORT).show();
+                    goMain();
                 }
             }
         });
@@ -55,6 +72,12 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    private void goMain(){
+        if(validate()){
+            Toast.makeText(LoginActivity.this,"ENTRAR EM BREVE!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public void loginFace(View v){
         Toast.makeText(this,"EM BREVE!!!", Toast.LENGTH_SHORT).show();
     }
@@ -65,6 +88,38 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public  void loginForgoten(View v){
-        Toast.makeText(LoginActivity.this,"LEMBRAR EM BREVE!!!", Toast.LENGTH_SHORT).show();
+        new MaterialDialog.Builder(this).title(R.string.forgot_pass)
+                .customView(R.layout.forgot_dialog, true)
+                .positiveText(R.string.forgot_ok).cancelable(true).positiveColor(
+                getResources().getColor(R.color.colorRedSignUp)).negativeColor(
+                getResources().getColor(R.color.colorRedSignUp)).negativeText(
+                R.string.forgot_cancel).onPositive(new MaterialDialog.SingleButtonCallback(){
+            @Override
+            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                dialog.dismiss();
+            }
+        }).onNegative(new MaterialDialog.SingleButtonCallback() {
+            @Override
+            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                dialog.dismiss();
+            }
+        }).show();
+    }
+
+    private boolean validate(){
+        String email = editEmail.getText().toString().trim();
+        String pass = editPass.getText().toString().trim();
+        boolean valid = true;
+        if(email.isEmpty()){
+            inputEmail.setErrorEnabled(true);
+            editEmail.setError(getString(R.string.err_msg));
+            valid = false;
+        }
+        if (pass.isEmpty()){
+            inputPass.setErrorEnabled(true);
+            editPass.setError(getString(R.string.err_msg));
+            valid = false;
+        }
+        return valid;
     }
 }
